0e9a2308b45e01e42a01a4238cdee7c953f74770
nasobray <nasser.alrayhi@gmail.com>
2021-06-26 15:09:27 +0400

    Added config example

---
+++ config.example.js
@@ -0,0 +1,39 @@
+var config = {};
+
+config.enableTelegramNotifications =  true; 
+
+config.telegramUsers = [];
+config.telegramUsers.push('TELEGRAM_USERID');
+config.telegramUsers.push('TELEGRAM_USERID');
+config.telegramUsers.push('TELEGRAM_USERID');
+
+config.telegramToken =  'TELEGRAM_KEY'; 
+
+
+config.landingZone = '/path/to/landing/zone/';
+
+
+config.destinationStorage = [];
+config.destinationStorage.push('/path/to/destinationStorage/');
+config.destinationStorage.push('/path/to/destinationStorage/');
+config.destinationStorage.push('192.168.0.1');
+
+
+config.fileExclusions = [];
+config.fileExclusions.push('.EXCLUDED_EXTENTIONS');
+config.fileExclusions.push('.EXCLUDED_EXTENTIONS');
+
+config.minimumDiskSizeInGB = 5;
+config.minimumFreeSizeInGB =  1; 
+
+config.MD5HashCalculation =  false; 
+
+
+config.enableFileReceiveServer =  true; 
+config.FileReceiveServerPort = 6969;
+config.RecieveBufferSize  = 2 * 1024 * 1024;
+config.SendBufferSize  = 1 * 1024 * 1024;
+
+
+
+module.exports = config;
